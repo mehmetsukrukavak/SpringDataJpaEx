ee390b7122d9a6a968c832d269229539ad018a9a
Mehmet Şükrü Kavak <mehmetsukru.kavak07@gmail.com>
2025-02-17 10:45:05 +0300

    findAll

--- src/main/java/com/mehmetsukrukavak/SpringDataJpaEx/SpringDataJpaExApplication.java
+++ src/main/java/com/mehmetsukrukavak/SpringDataJpaEx/SpringDataJpaExApplication.java
@@ -14,25 +14,27 @@ public class SpringDataJpaExApplication {
 
 		StudentRepo repo = context.getBean(StudentRepo.class);
 
-		Student student1 = context.getBean(Student.class);
-		Student student2 = context.getBean(Student.class);
-		Student student3 = context.getBean(Student.class);
-
-		student1.setId(101);
-		student1.setName("John Doe");
-		student1.setMarks(78);
-
-		student2.setId(102);
-		student2.setName("Jane Doe");
-		student2.setMarks(96);
-
-		student3.setId(103);
-		student3.setName("Mehmet Şükrü Kavak");
-		student3.setMarks(98);
+//		Student student1 = context.getBean(Student.class);
+//		Student student2 = context.getBean(Student.class);
+//		Student student3 = context.getBean(Student.class);
+//
+//		student1.setId(101);
+//		student1.setName("John Doe");
+//		student1.setMarks(78);
+//
+//		student2.setId(102);
+//		student2.setName("Jane Doe");
+//		student2.setMarks(96);
+//
+//		student3.setId(103);
+//		student3.setName("Mehmet Şükrü Kavak");
+//		student3.setMarks(98);
 
 		//repo.save(student1);
-		repo.save(student2);
-		repo.save(student3);
+		//repo.save(student2);
+		//repo.save(student3);
+
+		System.out.println(repo.findAll());
 
 	}
 
